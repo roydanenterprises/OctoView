3d6e38bd0ad8bd7b75b619a28f6a1d61b4fd8bcc
Kyle MacDonald <kmacdonald1983@gmail.com>
2018-05-01 14:25:58 -0500

    cleanup.

--- OctoView.Web/ClientApp/components/Feature.tsx
+++ OctoView.Web/ClientApp/components/Feature.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'; 
 
 interface IFeatureProperties {
-	name: string,
-	renderType : string
+	name: string;
+	renderType : string;
 }
 
 interface IFeatureState {
--- OctoView.Web/ClientApp/components/Features.tsx
+++ OctoView.Web/ClientApp/components/Features.tsx
@@ -11,7 +11,6 @@ interface IFeaturesState {
 	loading: boolean;
 }
 
-
 export class Features extends React.Component<RouteComponentProps<{}>, IFeaturesState> {
 	constructor() {
 		super();
