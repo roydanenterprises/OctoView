b1a7586f30c534c1ca615b5a50bdbbeea3b747ef
Kyle MacDonald <kmacdonald1983@gmail.com>
2018-05-01 09:39:30 -0500

    add console to do migrations for the github project.

---
+++ OctoView.Github.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OctoView.Github.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
