c918a5ced297e0dbfcc768f2f8edd596408ae26f
James McCollum <jmccollum@signet-studios.com>
2018-05-01 11:01:29 -0500

    Add migration for User repository list

---
+++ OctoView.Web/Controllers/GithubController.cs
@@ -0,0 +1,34 @@
+﻿using GithubDashboard.Github.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OctoView.Web.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OctoView.Web.Controllers
+{
+	[Route("api/github")]
+	public class GithubController : Controller
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly IGithubService _githubService;
+
+		public GithubController(UserManager<ApplicationUser> userManager,
+			IGithubService githubService)
+		{
+			_userManager = userManager;
+			_githubService = githubService;
+		}
+
+		[HttpGet("branches")]
+		public async Task<object> GetBranches()
+		{
+			var token = HttpContext.User.Claims.FirstOrDefault(x => x.Type == "GithubAccessToken")?.Value;
+
+			var allRepos = await _githubService.GetAllRepositories(token);
+			//var selectedRepos = await _userManager.GetGithubRepositories(_userManager.GetUserId(HttpContext.User));
+
+			return true;
+		}
+	}
+}
---
+++ OctoView.Web/Identity/ApplicationUserManager.cs
@@ -0,0 +1,6 @@
+﻿namespace OctoView.Web.Identity
+{
+	public class ApplicationUserManager
+	{
+	}
+}
