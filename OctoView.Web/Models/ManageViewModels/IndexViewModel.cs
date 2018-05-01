7dcaf4576875227af562d423151d7a4e5be99162
James McCollum <jmccollum@signet-studios.com>
2018-05-01 13:01:04 -0500

    Fix Manage View

--- OctoView.Web/Models/ManageViewModels/IndexViewModel.cs
+++ OctoView.Web/Models/ManageViewModels/IndexViewModel.cs
@@ -1,21 +1,29 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TestApplicationReact.Models.ManageViewModels
 {
-    public class IndexViewModel
-    {
-        public string Username { get; set; }
+	public class IndexViewModel
+	{
+		public string Username { get; set; }
+		public IList<UserLoginInfo> Logins { get; set; }
 
-        public bool IsEmailConfirmed { get; set; }
+		public bool IsEmailConfirmed { get; set; }
 
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
 
-        [Phone]
-        [Display(Name = "Phone number")]
-        public string PhoneNumber { get; set; }
+		[Phone]
+		[Display(Name = "Phone number")]
+		public string PhoneNumber { get; set; }
 
-        public string StatusMessage { get; set; }
-    }
+		public string StatusMessage { get; set; }
+		public bool HasPassword { get; set; }
+		public string GithubAccount { get; set; }
+		public List<string> GithubRepositories { get; set; }
+		public List<string> AvailableRepositories { get; set; }
+
+	}
 }
