b54d9de8bfd900414b1aaf3b1d316e3afc75ced4
James McCollum <jmccollum@signet-studios.com>
2018-05-01 11:40:02 -0500

    Add controller route to get branches

--- OctoView.Web/Controllers/GithubController.cs
+++ OctoView.Web/Controllers/GithubController.cs
@@ -1,8 +1,10 @@
 ﻿using GithubDashboard.Github.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using OctoView.Web.Hubs;
 using OctoView.Web.Models;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OctoView.Web.Controllers
@@ -11,13 +13,15 @@ namespace OctoView.Web.Controllers
 	public class GithubController : Controller
 	{
 		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly IUserStore<ApplicationUser> _userStore;
 		private readonly IGithubService _githubService;
 
 		public GithubController(UserManager<ApplicationUser> userManager,
-			IGithubService githubService)
+			IGithubService githubService, IUserStore<ApplicationUser> userStore)
 		{
 			_userManager = userManager;
 			_githubService = githubService;
+			_userStore = userStore;
 		}
 
 		[HttpGet("branches")]
@@ -26,9 +30,19 @@ namespace OctoView.Web.Controllers
 			var token = HttpContext.User.Claims.FirstOrDefault(x => x.Type == "GithubAccessToken")?.Value;
 
 			var allRepos = await _githubService.GetAllRepositories(token);
-			//var selectedRepos = await _userManager.GetGithubRepositories(_userManager.GetUserId(HttpContext.User));
+			var selectedRepos = (await _userStore.FindByIdAsync(_userManager.GetUserId(HttpContext.User), new CancellationToken()))?.Repositories.Select(x => x.RepositoryName)
+				.ToList();
 
-			return true;
+			var tasks = allRepos
+				.Where(x => selectedRepos.Any(y => y == x.FullName))
+				.AsParallel()
+				.Select(async x => await _githubService.CreateGithubBranches(token, x));
+
+			var result = (await Task.WhenAll(tasks)).SelectMany(x => x).ToList();
+
+			new Task(() => result.ForEach(x => GithubHub.BranchUpdated(x.Repo, x))).Start();
+
+			return result;
 		}
 	}
 }
