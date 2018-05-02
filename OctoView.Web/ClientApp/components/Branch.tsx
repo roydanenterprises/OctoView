4317d9aa4d71eac6c670354f76e9739d876ba4bd
Jim Cloudman <jim@jimcloudman.com>
2018-05-02 14:52:14 -0500

    Drawers expanding on toggle

--- OctoView.Web/ClientApp/components/Branch.tsx
+++ OctoView.Web/ClientApp/components/Branch.tsx
@@ -26,18 +26,24 @@ export interface IBranchState {
 export class Branch extends
 	React.Component<IBranch, any> {
 	componentWillMount() {
-		this.setState({ isOpen: false});
+		this.setState({ isOpen: false });
 	}
 	toggleState() {
 		this.setState({ isOpen: !this.state.isOpen });
 	}
 	render() {
+		
+		let className = 'ov-c-branch';
+		if (this.state.isOpen) {
+		  className += ' ov-c-branch--opened';
+		}
+
 		if (this.props.layout === 'table') {
 			return <div>
 				       <div>{this.props.repo}</div>
 			       </div>;
 		}
-		return <div onClick={() => this.toggleState()} className={"ov-c-branch " + this.state.isOpened ?"ov-c-branch--opened" : ""}>
+		return <div onClick={() => this.toggleState()} className={className}>
 					<div className="ov-c-branch__left-indicator"></div>
 			    	<div className="ov-c-branch__name">{this.props.branchName}</div>
 			<div className="ov-c-branch__approval-indicator"></div>
