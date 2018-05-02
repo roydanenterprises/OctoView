4d91a8b7f00972dc61ec82f374569f65ea370db3
Kyle MacDonald <kmacdonald1983@gmail.com>
2018-05-02 09:24:37 -0500

    revert changes back to stateful components to anticipate signalr.

--- OctoView.Web/ClientApp/components/Feature.tsx
+++ OctoView.Web/ClientApp/components/Feature.tsx
@@ -4,11 +4,11 @@ import {IPullRequest } from './PullRequest'
 export interface IFeature {
 	repo: string;
 	branchName: string;
-	layout : string;
+	layout: string;
 	pulls: IPullRequest[];
 }
 
-export const Feature: React.SFC<IFeature> = (props) => {
+/*export const Feature: React.SFC<IFeature> = (props) => {
 	if (props.layout === "table") {
 		return <div>
 			       <div>{props.repo}</div>
@@ -17,13 +17,18 @@ export const Feature: React.SFC<IFeature> = (props) => {
 	return <div>
 		       <div>{props.repo}</div>
 	       </div>;
-};
+};*/
 
-/*export class Feature extends
+export class Feature extends
 React.Component<IFeature, any> {
 	render() {
+		if (props.layout === 'table') {
+			return <div>
+				       <div>{props.repo}</div>
+			       </div>;
+		}
 		return <div>
-			       <div>{this.props.repo}</div>
+			       <div>{props.repo}</div>
 		       </div>;
 	}
-}*/
+}
